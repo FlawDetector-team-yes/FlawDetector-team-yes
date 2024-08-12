d3abe388ef6a951bea8f16b8cd984c9c5bf3cc43
leedo97y <dlehdus1433@gmail.com>
2024-08-12 14:48:57 +0900

    chore: #5 font 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
   );
