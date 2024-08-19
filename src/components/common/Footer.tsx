eec5c194f8f89dec8c57db517d21f9abd108586b
minho <kasamin98@naver.com>
2024-08-19 18:37:41 +0900

    refactor: #31 Footer 리팩토링 - 서비스 이용약관, 개인정보처리방침 주소 연결 - login에서는 보이지 않게 조건부 렌더링 처리 - 중복된 코드 합쳐서 가독성 보완

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,110 +1,94 @@
+"use client";
 import Image from "next/image";
 import sfacspace from "/public/images/sfacspace.svg";
-/**
- * @TODO 배경이미지 넣기
- * @TODO2 서비스 이용 약관 등 3가지 주소 바로 넣기
- */
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 function Footer() {
-  const mainBg = `bg-[url('../../public/images/MainBackground.svg')]`;
+  const currentPath = usePathname();
+  //텍스트 스타일
+  const textBaseClass =
+    "text-center font-['Inter'] text-base font-medium leading-normal";
+  const textGrayClass = `${textBaseClass} text-[#969696]`;
+  const textDarkClass = `${textBaseClass} text-[#3f3f3f]`;
+  console.log(currentPath);
+
   return (
     <>
-      <div
-        className={`bottom-0 left-0 right-0 flex justify-between px-20 py-10 ${mainBg} flex bg-cover bg-no-repeat`}
-      >
-        <div className="flex">
-          <Image
-            className="mr-6"
-            src={sfacspace}
-            alt="sfacspaceImg"
-            width={120}
-            height={55}
-          />
-          <div className="flex h-20 items-start justify-start gap-10">
-            <div className="flex flex-col items-start justify-start gap-1">
-              <div className="flex items-start justify-start gap-[26px]">
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696]">
-                  (주)스팩스페이스
+      {currentPath === "/login" || (
+        <footer
+          className={`bottom-0 left-0 right-0 flex justify-between bg-cover bg-no-repeat px-20 py-10`}
+          style={{ backgroundImage: "url('/images/MainBackground.svg')" }}
+        >
+          <div className="flex">
+            <Image
+              src={sfacspace}
+              alt="sfacspaceImg"
+              width={120}
+              height={55}
+              className="mr-6"
+            />
+
+            <div className="flex h-20 items-start gap-10">
+              <div className="flex flex-col gap-1">
+                <div className="flex items-start gap-[26px]">
+                  <p className={textGrayClass}>(주)스팩스페이스</p>
+                  <div className="flex items-start gap-[11px]">
+                    <p className={textGrayClass}>대표자</p>
+                    <p className={textDarkClass}>염민호</p>
+                  </div>
+                </div>
+                <p className={textDarkClass}>
+                  서울 강서구 마곡중앙2로 11, 3층 303호
                 </p>
-                <div className="flex items-start justify-start gap-[11px]">
-                  <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696]">
-                    대표자
-                  </p>
-                  <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#3f3f3f]">
-                    염민호
-                  </p>
+                <div className="flex items-start gap-[23px]">
+                  <p className={textGrayClass}>Email</p>
+                  <p className={textDarkClass}>admin@sfacspace.com</p>
                 </div>
               </div>
-              <div className="text-center font-['Inter'] text-base font-medium leading-normal text-[#3f3f3f]">
-                서울 강서구 마곡중앙2로 11, 3층 303호
-              </div>
-              <div className="flex items-start justify-start gap-[23px]">
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696]">
-                  Email
-                </p>
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#3f3f3f]">
-                  admin@sfacspace.com
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col items-start justify-start gap-1">
-              <div className="flex items-start justify-start gap-[7px]">
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696]">
-                  사업자등록번호
-                </p>
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#3f3f3f]">
-                  450-87-01864
-                </p>
-              </div>
-              <div className="flex items-start justify-start gap-[13px]">
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696]">
-                  대표전화
-                </p>
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#3f3f3f]">
-                  02-6217-1119
-                </p>
-              </div>
-              <div className="flex items-start justify-start gap-[30px]">
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696]">
-                  팩스
-                </p>
-                <p className="text-center font-['Inter'] text-base font-medium leading-normal text-[#3f3f3f]">
-                  02-6217-1115
-                </p>
+
+              <div className="flex flex-col gap-1">
+                <div className="flex items-start gap-[7px]">
+                  <p className={textGrayClass}>사업자등록번호</p>
+                  <p className={textDarkClass}>450-87-01864</p>
+                </div>
+                <div className="flex items-start gap-[13px]">
+                  <p className={textGrayClass}>대표전화</p>
+                  <p className={textDarkClass}>02-6217-1119</p>
+                </div>
+                <div className="flex items-start gap-[30px]">
+                  <p className={textGrayClass}>팩스</p>
+                  <p className={textDarkClass}>02-6217-1115</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col justify-end">
-          <div className="flex h-[66px] flex-col items-end justify-start gap-3">
-            <div className="flex items-start justify-start gap-8">
-              <a
-                href="https://www.google.com/"
-                className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696] underline"
-              >
-                회사소개
-              </a>
-              <a
-                href="https://www.google.com/"
-                className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696] underline"
-              >
-                서비스이용약관
-              </a>
-              <a
-                href="https://www.google.com/"
-                className="text-center font-['Inter'] text-base font-medium leading-normal text-[#969696] underline"
-              >
-                개인정보처리방침
-              </a>
-            </div>
-            <div className="text-center">
-              <p className="font-['Inter'] text-base font-medium leading-normal text-[#3f3f3f]">
-                ⒸSpacspace.All right reserved.
+
+          <div className="flex flex-col justify-end">
+            <div className="flex h-[66px] flex-col items-end gap-3">
+              <div className="flex items-start gap-8">
+                <Link
+                  href="/doc/terms-of-service"
+                  className={`${textGrayClass} underline`}
+                >
+                  서비스이용약관
+                </Link>
+                <Link
+                  href="/doc/privacy-policy"
+                  className={`${textGrayClass} underline`}
+                >
+                  개인정보처리방침
+                </Link>
+              </div>
+              <p className={`${textDarkClass} text-center`}>
+                ⒸSpacspace. All rights reserved.
               </p>
             </div>
           </div>
-        </div>
-      </div>
+        </footer>
+      )}
     </>
   );
 }
+
 export default Footer;
