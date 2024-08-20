4c78355caad8bc8610daa1001ef1b3312d5f2a5f
minho <kasamin98@naver.com>
2024-08-20 19:35:46 +0900

    refactor: 모달 닫기 버튼 공용으로 생성(...rest추가)

--- src/components/common/button/CloseModalButton.tsx
+++ src/components/common/button/CloseModalButton.tsx
@@ -6,14 +6,19 @@ type CloseModalProps = {
   buttonText: string;
   className: string;
   onClick?: () => void;
-};
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 /**
  * 모달 닫는 버튼로직을 연결해둔 버튼입니다.
  * @param buttonText 버튼에 들어갈 텍스트 값
  * @param className 기존 tailwind css처럼 값 넣어주시면 됩니다.(기본 디자인 아무것도 없음)
  * @param onClick 버튼 종료시 넣어줄 기능.
  */
-function CloseModalButton({ buttonText, className, onClick }: CloseModalProps) {
+function CloseModalButton({
+  buttonText,
+  className,
+  onClick,
+  ...rest
+}: CloseModalProps) {
   const { setIsClose } = useModal();
 
   const handleCloseModal = () => {
@@ -22,7 +27,11 @@ function CloseModalButton({ buttonText, className, onClick }: CloseModalProps) {
   };
 
   return (
-    <button className={`${className} text-center`} onClick={handleCloseModal}>
+    <button
+      className={`${className} text-center`}
+      onClick={handleCloseModal}
+      {...rest}
+    >
       {buttonText}
     </button>
   );
