76d38ddfe13e6a7e57f94fc736c91270557f4713
minho <kasamin98@naver.com>
2024-08-26 20:50:01 +0900

    chore: 테일윈드 모듈 content추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={`${inter.className} relative`}>
         <Gnb />
         <ModalProvider />
