046903b30c12ede8cd13cbb0eed2526a7479f228
minho <kasamin98@naver.com>
2024-08-27 08:52:11 +0900

    fix: 테일윈드 설정 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,9 @@ import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/store/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
