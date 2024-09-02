b37967d105b79373e2987addcd66d48c523f0a9a
leedo97y <dlehdus1433@gmail.com>
2024-09-02 14:06:34 +0900

    feat: #54 llama 코드검사 generate 관련 API 코드 추가

--- src/app/api/analyze/llm/route.ts
+++ src/app/api/analyze/llm/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
+    const body = await req.json();
+    const { content } = body;
+
     // token 구하기
     const formdata = new FormData();
     formdata.append("username", `${process.env.NEXT_PUBLIC_USERNAME}`);
@@ -18,6 +21,29 @@ export async function POST(req: Request) {
       `http://43.203.238.76:8000/auth/token`,
       tokenOption,
     ).then((res) => res.json());
+
+    // console.log(content);
+
+    const option = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token.access_token}`,
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store",
+      },
+      body: JSON.stringify({
+        user_message: `${content}\n\nquestion: Please translate the result of analyzing the security vulnerability of the above code into Korean and respond only in Korean.`,
+        temperature: 0.6,
+        top_p: 0.6,
+      }),
+    };
+    const result = await fetch(
+      `http://43.203.238.76:8000/generate`,
+      option,
+    ).then((res) => res.text());
+    console.log(result);
+
+    return NextResponse.json(result);
   } catch (e) {
     console.log(e);
   }
