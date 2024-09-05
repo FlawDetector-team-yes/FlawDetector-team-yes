57d786e2a5e0586ae5dff9e950de3610da63c8fd
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-09-05 14:18:41 +0900

    feat : #64 마이페이지 사용자 repo 데이터 반영

--- src/components/me/GitListItem.tsx
+++ src/components/me/GitListItem.tsx
@@ -1,16 +1,23 @@
 "use client";
 import { useEffect, useState } from "react";
 import menuDot from "/public/images/menu-dot.png";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import plus from "/public/images/plus.png";
 import LibrarySort from "./LibrarySort";
-// /me github repository 아이템요소 컴포넌트
-export default function GitListItem() {
-  // 17개의 요소를 가진 배열 생성
-  let array = new Array(17);
+import useUserStore from "@/store/useUserStore";
 
+type TUserInfoType = {
+  label: string;
+  name: string;
+  createdAt: string;
+};
+
+export default function GitListItem() {
+  const user = useUserStore((state) => state.userInfo);
+  let array = new Array(17);
+  const [repos, setRepos] = useState<TUserInfoType[]>([]);
   // 배열의 길이를 기준으로 표시할 항목의 총 높이 계산
-  let arrLength = Math.ceil((array.length * 200) / 3);
+  let arrLength = Math.ceil((repos.length * 200) / 3);
 
   // 상태 초기화: 컴포넌트 높이, overflow 스타일, 계산된 높이, '더보기' 버튼 표시 여부
   const [styleHeight, setStyleHeight] = useState<number | string>(710);
@@ -18,22 +25,20 @@ export default function GitListItem() {
   const [count, setCount] = useState<number | string>(arrLength);
   const [showMore, setShowMore] = useState(false);
 
-  //삭제,공유 각 인덱스에 맞춰 활성화 해야하기때문에 초기값은 null
-  const [miniModal, setMiniModal] = useState<number | null>(null);
-  // 컴포넌트가 마운트될 때 배열의 길이를 확인하고 '더보기' 버튼 표시 여부를 결정
-  useEffect(() => {
-    if (12 < array.length) {
-      setShowMore(!showMore);
+  const fetchUserRepos = async () => {
+    try {
+      if (user) {
+        const res = await await (
+          await fetch(`api/github/repos/${user.owner}`)
+        ).json();
+        // {name, createdAt}
+        setRepos(res);
+        console.log(JSON.stringify(res));
+      }
+    } catch (e) {
+      console.log(e);
     }
-  }, []);
-  useEffect(() => {
-    if (styleHeight < count) {
-      setStyleOverflow("hidden");
-      // 그렇지 않으면 overflow를 보이게 하고 높이를 auto로 설정, "더보기" 버튼을 숨김
-    } else {
-      setStyleOverflow("visible");
-    }
-  }, [moreHandler]);
+  };
 
   // '더보기' 버튼을 클릭했을 때 호출되는 함수
   function moreHandler() {
@@ -46,10 +51,23 @@ export default function GitListItem() {
       setStyleHeight("auto");
     }
   }
-  function miniModalHandler(index: number) {
-    // 클릭한 인덱스가 현재 열려 있는 모달의 인덱스와 같으면 닫고, 다르면 열리게 설정
-    setMiniModal((prev) => (prev === index ? null : index));
-  }
+
+  useEffect(() => {
+    // 사용자 git repos
+    fetchUserRepos();
+    if (12 < repos.length) {
+      setShowMore(!showMore);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (styleHeight < count) {
+      setStyleOverflow("hidden");
+    } else {
+      setStyleOverflow("visible");
+    }
+  }, [moreHandler]);
+
   return (
     <>
       <LibrarySort />
@@ -60,41 +78,22 @@ export default function GitListItem() {
         }}
         className={`grid grid-cols-4`}
       >
-        {array
-          .fill({
-            label: "label",
-            imgSrc: menuDot,
-            title: "Folder name",
-            sub: "sub title",
-          })
-          .map((item, index) => (
-            <div
-              key={index}
-              className="mt-[30px] flex h-[200px] w-[310px] flex-col justify-between rounded-xl border border-solid border-[#E0CEFF] px-2 py-4"
-            >
-              <div className="relative flex h-[30px] justify-between">
-                <div className="h-[23px] w-[43px] rounded-full border border-solid border-[#3F3F3F] px-1 py-[2px] text-center text-[12px]">
-                  label
-                </div>
-                <div
-                  className="w-[10px] cursor-pointer"
-                  onClick={() => miniModalHandler(index)}
-                >
-                  <Image src={item.imgSrc} alt="삼단바" width={3} height={17} />
-                </div>
-                {miniModal === index && (
-                  <div className="absolute right-[-1px] top-5 flex h-[94px] w-[70px] flex-col justify-center gap-4 rounded-lg bg-[#ffffff] text-[16px] font-medium shadow-md">
-                    <span className="w-full text-center">삭제</span>
-                    <span className="w-full text-center">공유</span>
-                  </div>
-                )}
-              </div>
-              <div className="flex flex-col">
-                <span className="text-[24px]">{item.title}</span>
-                <span className="font-light">{item.sub}</span>
+        {repos.map((item, index) => (
+          <div
+            key={index}
+            className="mt-[30px] flex h-[200px] w-[310px] flex-col justify-between rounded-xl border border-solid border-[#E0CEFF] px-2 py-4"
+          >
+            <div className="relative flex h-[30px] justify-between">
+              <div className="h-[23px] w-[43px] rounded-full border border-solid border-[#3F3F3F] px-1 py-[2px] text-center text-[12px]">
+                label
               </div>
             </div>
-          ))}
+            <div className="flex flex-col">
+              <span className="text-[24px]">{item.name}</span>
+              <span className="font-light">{item.createdAt}</span>
+            </div>
+          </div>
+        ))}
       </div>
       {styleOverflow === "hidden" && (
         <div className="mt-10 flex w-full justify-center">
@@ -107,6 +106,7 @@ export default function GitListItem() {
               <Image src={plus} alt="더보기" width={18} height={18} />
             </div>
           </button>
+          <div>{JSON.stringify(repos)}</div>
         </div>
       )}
     </>
