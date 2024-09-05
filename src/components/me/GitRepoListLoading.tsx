6606e9b24eb315dc958246a5d80a076940cb323f
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-09-05 18:24:31 +0900

    feat : #64 마이페이지 레포 목록에 스켈레톤 UI 추가 및 적용

---
+++ src/components/me/GitRepoListLoading.tsx
@@ -0,0 +1,21 @@
+export default function GitRepoListLoading() {
+  return (
+    <>
+      {Array(1)
+        .fill(null)
+        .map((_, index) => (
+          <div
+            key={index}
+            className="flex h-[225px] w-[310px] animate-pulse flex-col justify-between rounded-3xl border border-solid border-[#cecece] p-[20px]"
+          >
+            <div className="flex h-[100px] w-[270px] flex-col gap-2 rounded-xl bg-gray-400"></div>
+            <div className="flex items-end justify-between">
+              <button
+                className={`flex h-[45px] w-[146px] items-center justify-evenly rounded-xl bg-gray-400`}
+              ></button>
+            </div>
+          </div>
+        ))}
+    </>
+  );
+}
