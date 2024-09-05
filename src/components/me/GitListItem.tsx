0317caedb73d0c6c174fc67be43c3df093895baa
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-09-05 15:07:51 +0900

    refactor : #64 더보기 버튼 로직 slice로 나눠서 보일 수 있도록 변경 및 리팩토링

--- src/components/me/GitListItem.tsx
+++ src/components/me/GitListItem.tsx
@@ -14,24 +14,24 @@ type TUserInfoType = {
 
 export default function GitListItem() {
   const user = useUserStore((state) => state.userInfo);
-  let array = new Array(17);
   const [repos, setRepos] = useState<TUserInfoType[]>([]);
-  // 배열의 길이를 기준으로 표시할 항목의 총 높이 계산
-  let arrLength = Math.ceil((repos.length * 200) / 3);
+  const [visibleCount, setVisibleCount] = useState<number>(12); // 초기 12개만 출력
 
-  // 상태 초기화: 컴포넌트 높이, overflow 스타일, 계산된 높이, '더보기' 버튼 표시 여부
-  const [styleHeight, setStyleHeight] = useState<number | string>(710);
-  const [styleOverflow, setStyleOverflow] = useState("hidden");
-  const [count, setCount] = useState<number | string>(arrLength);
-  const [showMore, setShowMore] = useState(false);
+  // repos 길이가 12개 이하일 경우, 더보기 버튼을 숨김
+  const isMoreItems = repos.length > visibleCount;
 
+  // 더보기 버튼 클릭 시, 12개씩 추가로 보여주기
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 12);
+  };
+
+  // 사용자 github repo 목록 불러오기
   const fetchUserRepos = async () => {
     try {
       if (user) {
         const res = await await (
           await fetch(`api/github/repos/${user.owner}`)
         ).json();
-        // {name, createdAt}
         setRepos(res);
         console.log(JSON.stringify(res));
       }
@@ -40,73 +40,46 @@ export default function GitListItem() {
     }
   };
 
-  // '더보기' 버튼을 클릭했을 때 호출되는 함수
-  function moreHandler() {
-    // 현재 높이가 count보다 작으면 높이를 증가시키고 overflow를 숨김 상태로 유지
-    if (styleHeight < count) {
-      setStyleHeight((prev) => (typeof prev === "number" ? prev + 710 : prev));
-    }
-    // 그렇지 않으면 overflow를 보이게 하고 높이를 'auto'로 설정
-    else {
-      setStyleHeight("auto");
-    }
-  }
-
   useEffect(() => {
-    // 사용자 git repos
     fetchUserRepos();
-    if (12 < repos.length) {
-      setShowMore(!showMore);
-    }
   }, [user]);
-
-  useEffect(() => {
-    if (styleHeight < count) {
-      setStyleOverflow("hidden");
-    } else {
-      setStyleOverflow("visible");
-    }
-  }, [moreHandler]);
 
   return (
     <>
       <LibrarySort />
-      <div
-        style={{
-          height: styleHeight < count ? `${styleHeight}px` : "auto",
-          overflow: styleOverflow,
-        }}
-        className={`grid grid-cols-4`}
-      >
-        {repos.map((item, index) => (
+      <div className="grid grid-cols-4 gap-10 pt-5">
+        {repos.slice(0, visibleCount).map((repo, index) => (
           <div
             key={index}
-            className="mt-[30px] flex h-[200px] w-[310px] flex-col justify-between rounded-xl border border-solid border-[#E0CEFF] px-2 py-4"
+            className="flex h-[200px] w-[310px] flex-col justify-between rounded-xl border border-solid border-[#E0CEFF] px-2 py-4"
           >
-            <div className="relative flex h-[30px] justify-between">
+            <div className="flex h-[30px] flex-col justify-between gap-2">
               <div className="h-[23px] w-[43px] rounded-full border border-solid border-[#3F3F3F] px-1 py-[2px] text-center text-[12px]">
                 label
               </div>
+              <span className="w-[296px] break-words text-[24px]">
+                {repo.name}
+              </span>
             </div>
             <div className="flex flex-col">
-              <span className="text-[24px]">{item.name}</span>
-              <span className="font-light">{item.createdAt}</span>
+              <span className="font-light">{repo.createdAt}</span>
             </div>
           </div>
         ))}
       </div>
-      {styleOverflow === "hidden" && (
-        <div className="mt-10 flex w-full justify-center">
+
+      {/* 더보기 버튼 */}
+      {isMoreItems && (
+        <div className="mt-7 flex w-full justify-center">
           <button
             className="flex h-[54px] w-[103px] items-center justify-center rounded-lg border border-solid border-[#6100FF] text-[#6100FF]"
-            onClick={moreHandler}
+            onClick={handleLoadMore}
           >
             <span>more </span>
             <div>
               <Image src={plus} alt="더보기" width={18} height={18} />
             </div>
           </button>
-          <div>{JSON.stringify(repos)}</div>
         </div>
       )}
     </>
