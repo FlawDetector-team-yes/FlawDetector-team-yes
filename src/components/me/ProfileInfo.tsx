d7976892896ba8f31c5fdb02b5eed6cb9713b2e3
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-09-08 14:03:20 +0900

    fix : #64 프로필 이미지 로딩 버벅거리는 오류 해결

--- src/components/me/ProfileInfo.tsx
+++ src/components/me/ProfileInfo.tsx
@@ -9,17 +9,12 @@ import useUserStore from "@/store/useUserStore";
 
 export default function ProfileInfo() {
   const userData = useUserStore((state) => state.userInfo);
+  const userProfileImg = useUserStore((state) => state.userInfo?.profileImg);
   const isLoading = useUserStore((state) => state.isLoading);
   const setUserData = useUserStore((state) => state.setUserData);
   const setIsLoading = useUserStore((state) => state.setIsLoading);
   const { data: session } = useSession();
-  const [isImgLoading, setIsImgLoading] = useState<boolean>(true);
-  const [userProfileImg, setUserProfileImg] = useState("");
   const pathName = usePathname();
-
-  const handleLoadImg = () => {
-    setIsImgLoading(false);
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -38,7 +33,6 @@ export default function ProfileInfo() {
           profileImg: session.user?.image,
           owner: owner,
         });
-        setUserProfileImg(userData?.profileImg as string);
         setIsLoading();
       }
     };
@@ -50,20 +44,19 @@ export default function ProfileInfo() {
       <div className="flex w-full justify-between border-b-2 pb-[60px]">
         <div className="flex gap-6 rounded-full">
           {/* 프로필 이미지 */}
-          {isImgLoading && (
+          {userProfileImg ? (
+            <Image
+              className={`rounded-full transition-opacity duration-500 ${
+                userProfileImg ? "opacity-100" : "opacity-0"
+              }`}
+              src={userProfileImg}
+              alt="Profile"
+              width={70}
+              height={70}
+            />
+          ) : (
             <div className="h-[70px] w-[70px] animate-pulse rounded-full bg-gray-400"></div>
           )}
-
-          <Image
-            className={`rounded-full transition-opacity duration-500 ${
-              isImgLoading ? "opacity-0" : "opacity-100"
-            }`}
-            onLoad={handleLoadImg}
-            src={userProfileImg}
-            alt="Profile"
-            width={70}
-            height={70}
-          />
 
           <div className="flex flex-col text-2xl">
             {!isLoading && (
