5963ca94f77fba34a1323c280c2dfedfea26a040
leedo97y <dlehdus1433@gmail.com>
2024-09-13 15:54:11 +0900

    fix: #107 개인정보처리방침, 서비스이용약관 ul 태그 div 태그로 수정, li 태그 pre 태그로 수정

--- src/app/doc/privacy-policy/page.tsx
+++ src/app/doc/privacy-policy/page.tsx
@@ -28,42 +28,44 @@ function PrivacyPolicyPage() {
         </pre>
       </div>
       <div className="border-b border-stroke-10 pb-10">
-        <ul className="flex flex-col gap-1 indent-5 text-sm">
-          <li>1. 개인정보의 수집 및 이용 목적, 항목 및 수집방법</li>
-          <li>2. 개인정보의 제공 및 처리위탁</li>
-          <li>3. 개인정보 수집·이용의 거부 시 불이익</li>
-          <li>4. 개인정보 보유 및 이용기간</li>
-          <li>5. 개인정보의 파기절차 및 방법</li>
-          <li>6. 회원의 권리와 행사 방법</li>
-          <li>7. 개인정보 자동 수집 장치의 설치/운영 및 거부에 관한 사항</li>
-          <li>8. 개인정보 보호를 위한 기술적/관리적 대책</li>
-          <li>9. 개인정보 관리 책임자의 성명, 연락처, 부서</li>
-          <li>10. 권익침해 구제방법</li>
-          <li>11. 고지의 의무</li>
-        </ul>
+        <div className="flex flex-col gap-1 indent-5 text-sm">
+          <pre>1. 개인정보의 수집 및 이용 목적, 항목 및 수집방법</pre>
+          <pre>2. 개인정보의 제공 및 처리위탁</pre>
+          <pre>3. 개인정보 수집·이용의 거부 시 불이익</pre>
+          <pre>4. 개인정보 보유 및 이용기간</pre>
+          <pre>5. 개인정보의 파기절차 및 방법</pre>
+          <pre>6. 회원의 권리와 행사 방법</pre>
+          <pre>7. 개인정보 자동 수집 장치의 설치/운영 및 거부에 관한 사항</pre>
+          <pre>8. 개인정보 보호를 위한 기술적/관리적 대책</pre>
+          <pre>9. 개인정보 관리 책임자의 성명, 연락처, 부서</pre>
+          <pre>10. 권익침해 구제방법</pre>
+          <pre>11. 고지의 의무</pre>
+        </div>
       </div>
       <div className="flex flex-col gap-5 border-b border-stroke-10 pb-10 pt-5 text-sm">
         <h2 className="text-xl font-semibold">
           1. 개인정보 수집의 이용 목적, 항목 및 수집방법
         </h2>
-        <ul className="flex flex-col gap-1 indent-5">
-          <li>
+        <div className="flex flex-col gap-1 indent-5">
+          <pre>
             1. “개인정보”란 생존하는 개인에 관한 정보로서 당해 개인을 식별할 수
             있는 정보를 말합니다.
-          </li>
-          <li>2. "회사"는 수집된 개인정보를 다음의 목적을 위해 활용합니다. </li>
-        </ul>
-        <ul className="flex flex-col gap-1 indent-10">
-          <li>
+          </pre>
+          <pre>
+            2. "회사"는 수집된 개인정보를 다음의 목적을 위해 활용합니다.{" "}
+          </pre>
+        </div>
+        <div className="flex flex-col gap-1 indent-10">
+          <pre>
             a. 회원가입 및 의사소통에 관한 사항: 회원 본인확인, 부정 가입방지,
             가입 의사확인, 고충상담 및 처리, 불만처리 등 민원처리 등
-          </li>
-          <li>b. 서비스 제공에 관한 사항: 코드 검사 서비스 제공</li>
-          <li>
+          </pre>
+          <pre>b. 서비스 제공에 관한 사항: 코드 검사 서비스 제공</pre>
+          <pre>
             c. 서비스 개선, 신규 서비스 개발, 서비스의 유효성 확인, 서비스 분석
             및 서비스 이용환경 개선
-          </li>
-        </ul>
+          </pre>
+        </div>
       </div>
       <div className="flex flex-col gap-5 pt-5 text-sm">
         <h2 className="text-xl font-semibold">2. 권익침해 구제방법</h2>
@@ -77,17 +79,17 @@ function PrivacyPolicyPage() {
             이 밖에 기타 개인정보침해의 신고, 상담에 대하여는 아래의 기관에
             문의하시기 바랍니다.
           </pre>
-          <ul className="flex flex-col gap-1 indent-5">
-            <li>
+          <div className="flex flex-col gap-1 indent-5">
+            <pre>
               1. 개인정보분쟁조정위원회 : (국번없이) 1833-6972
               (www.kopico.go.kr)
-            </li>
-            <li>
+            </pre>
+            <pre>
               2. 개인정보침해신고센터 : (국번없이) 118 (privacy.kisa.or.kr)
-            </li>
-            <li>3. 대검찰청 : (국번없이) 1301 (www.spo.go.kr)</li>
-            <li>4. 경찰청 : (국번없이) 182 (ecrm.cyber.go.kr)</li>
-          </ul>
+            </pre>
+            <pre>3. 대검찰청 : (국번없이) 1301 (www.spo.go.kr)</pre>
+            <pre>4. 경찰청 : (국번없이) 182 (ecrm.cyber.go.kr)</pre>
+          </div>
         </div>
         <div className="flex flex-col gap-5">
           <h2 className="pt-5 text-xl font-semibold">3. 고지의 의무</h2>
