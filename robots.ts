d92fab7bb06c900fe9ff212db3942e1755d6e9d1
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-09-18 22:07:52 +0900

    feat : #124 검색 엔진 활성화를 위한 sitemap.ts, robots.ts 생성 및 추가

---
+++ robots.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: "https://flaw-detector-team-yes-seven.vercel.app//sitemap.xml",
+  };
+}
+
+/*
+User-Agent: *
+Allow: /
+Disallow: /private/
+Sitemap: https://flaw-detector-team-yes-seven.vercel.app//sitemap.xml
+*/
