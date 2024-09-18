12958e1c16f46173ebf15b07b83d23613bef7a0e
minho <kasamin98@naver.com>
2024-09-18 14:51:41 +0900

    feat: #62 로그인 상태 처리를 위한 getSession값과 Clip 컴포넌트 생성 및 분할 - 실시간 시간 Hydration오류 수정을 위한 const -> let으로 수정 - 재사용을 위한 Clip 컴포넌트 분할 - 취약점 상세 보고난 후 뒤로 올 때 1페이지로 가는 오류 수정

---
+++ src/components/common/Clip.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+import pin from "/public/images/pin.png";
+import pinActive from "/public/images/pin-active.png";
+import { useSession } from "next-auth/react";
+import useFetchPin from "@/firebase/useFetchPin";
+import { usePinnedStore } from "@/store/usePinnedStore";
+
+/**
+ * @typedef {Object} TPinState
+ * @property {number} id - 핀 상태를 추적하는 항목의 ID.
+ * @property {boolean} state - 항목이 핀된 상태인지 여부를 나타냅니다.
+ */
+
+type TPinState = {
+  id: string;
+  state: boolean;
+};
+
+function Clip({ id }: { id: string }) {
+  const { pinnedId, togglePinnedId } = usePinnedStore();
+  const { data: session } = useSession();
+  console.log(pinnedId);
+
+  /**
+   * 핀 상태를 확인하는 함수입니다.
+   *
+   * @param {string} id - 핀 상태를 확인할 기사 항목의 ID.
+   * @returns {boolean} - 해당 ID가 pinnedId 배열에 있으면 true, 없으면 false.
+   */
+  const isPinned = () => {
+    return pinnedId.includes(id); // id가 pinnedId 배열에 포함되어 있으면 true 반환
+  };
+
+  const handlePinState = async (id: string, session: any) => {
+    togglePinnedId(id); // 핀 상태를 토글
+    await useFetchPin(id, session); // 핀 상태를 서버와 동기화
+  };
+
+  return (
+    <button onClick={() => handlePinState(id, session)}>
+      {isPinned() ? (
+        <Image src={pinActive} alt="pin-active" width={20} height={20} />
+      ) : (
+        <Image src={pin} alt="pin" width={20} height={20} />
+      )}
+    </button>
+  );
+}
+
+export default Clip;
