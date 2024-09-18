a939595ea899fe9fb5ee834c1b54439f21401b19
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-09-18 22:59:50 +0900

    fix : #107 배포 오류 수정(페이지 클라이언트 컴포넌트 키워드 삭제)

--- src/app/me/clip/page.tsx
+++ src/app/me/clip/page.tsx
@@ -1,9 +1,7 @@
-"use client";
 import Link from "next/link";
 import Image from "next/image";
 import leftArrow from "/public/images/left-violet-arrow.png";
 import { Inter } from "next/font/google";
-import ProfileInfo from "@/components/me/ProfileInfo";
 import ClippingArticleItem from "@/components/me/ClippingArticleItem";
 import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
@@ -29,7 +27,6 @@ export default function ClipPage() {
               </button>
             </Link>
           </div>
-
           <ClippingArticleItem />
         </div>
       </div>
